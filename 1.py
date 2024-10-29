267eb16fec456a5288934834b31dc2a38bf00e8e
LiuYifei <arnoliu20161225@126.com>
2024-10-29 21:18:18 +0800

    看所有场馆

--- 1.py
+++ 1.py
@@ -3,9 +3,9 @@ import datetime
 import pytz
 import json
 import pandas as pd
+import time
 
 def post_request():
-    base_url = "https://oa.shanghaitech.edu.cn/formmode/tree/treebrowser/CustomTreeBrowserAjax.jsp?dataconditionParam=_sfn_31901_sfv_{}_sfn_31902_sfv_{}_sfn_32340_sfv_4&id=63"
     
     # 获取今天的日期（UTC+8时区）
     tz = pytz.timezone('Asia/Shanghai')
@@ -21,11 +21,12 @@ def post_request():
         "User-Agent":"Mozilla/5.0"
     }
     
-    # 发送的数据
-    data = {
-        "pid": "63_6",
-        "name": "\u5ba4\u5185\u7fbd\u6bdb\u7403\u573a"  # 室内羽毛球场的Unicode编码
-    }
+    # 场地信息列表
+    venues = [
+        {"pid": "63_6", "name": "\u5ba4\u5185\u7fbd\u6bdb\u7403\u573a", "display_name": "🏸羽毛球场", "url":"https://oa.shanghaitech.edu.cn/formmode/tree/treebrowser/CustomTreeBrowserAjax.jsp?dataconditionParam=_sfn_31901_sfv_{}_sfn_31902_sfv_{}_sfn_32340_sfv_4&id=63"},
+        {"pid": "63_7", "name": "\u5ba4\u5185\u4e52\u4e53\u7403\u573a", "display_name": "🏓乒乓球场", "url":"https://oa.shanghaitech.edu.cn/formmode/tree/treebrowser/CustomTreeBrowserAjax.jsp?dataconditionParam=_sfn_31901_sfv_{}_sfn_31902_sfv_{}_sfn_32340_sfv_4&id=63"},
+        {"pid": "63_8", "name": "%E5%AE%A4%E5%A4%96%E7%BD%91%E7%90%83%E5%9C%BA", "display_name": "🎾网球场", "url":"https://oa.shanghaitech.edu.cn/formmode/tree/treebrowser/CustomTreeBrowserAjax.jsp?dataconditionParam=_sfn_31901_sfv_{}_sfn_31902_sfv_{}_sfn_32340_sfv_6&id=63"}
+    ]
     
     # 时间段列表
     time_slots = [
@@ -33,40 +34,54 @@ def post_request():
         "16:00-17:00", "17:00-18:00", "18:00-19:00", "19:00-20:00", "20:00-21:00", "21:00-22:00"
     ]
     
-    # 创建一个DataFrame来存储空闲时间段
-    availability_table = pd.DataFrame(columns=["Date"] + time_slots)
+    # 创建一个DataFrame来存储所有场地的空闲时间段
+    availability_tables = {}
     
-    # 请求从今天开始的5天
-    for i in range(5):
-        request_date = today + datetime.timedelta(days=i + 1)
-        formatted_date = request_date.strftime("%Y-%m-%d")
-        availability = {"Date": formatted_date}
-
-        # 遍历每个时间段
-        for index, time_slot in enumerate(time_slots):
-            url = base_url.format(formatted_date, index + 2)  # sfv_2_sfn字段对应时间段的index
-            try:
-                response = requests.post(url, headers=headers, data=data, cookies={"shkjdx_session": session_value}, timeout=1000)
-                response.raise_for_status()  # 检查请求是否成功
-                response.encoding = 'utf-8'
-                res = response.text.strip()
-                res = json.loads(res)
-                
-                # 判断该时间段是否有空位
-                if len(res) > 0 and "羽毛球场地" in res[0]["name"]:
-                    availability[time_slot] = "有"
-                else:
-                    availability[time_slot] = ""
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred for date {formatted_date}, time slot {time_slot}:", e)
-                availability[time_slot] = "Error"
+    # 请求每个场地从今天开始的5天
+    for venue in venues:
+        availability_table = pd.DataFrame(columns=["Date"] + time_slots)
         
-        # 将当天的空闲情况添加到表格中
-        availability_table.loc[len(availability_table)] =availability
+        for i in range(2):
+            request_date = today + datetime.timedelta(days=i + 1)
+            formatted_date = request_date.strftime("%Y-%m-%d")
+            availability = {"Date": formatted_date}
+
+            # 遍历每个时间段
+            for index, time_slot in enumerate(time_slots):
+                url = venue["url"].format(formatted_date, index + 2)  # sfv_2_sfn字段对应时间段的index
+                try:
+                    # 发送的数据
+                    data = {
+                        "pid": venue["pid"],
+                        "name": venue["name"]
+                    }
+                    
+                    response = requests.post(url, headers=headers, data=data, cookies={"shkjdx_session": session_value}, timeout=2)
+                    time.sleep(0.1)
+                    response.raise_for_status()  # 检查请求是否成功
+                    response.encoding = 'utf-8'
+                    res = response.text.strip()
+                    res = json.loads(res)
+                    
+                    # 判断该时间段是否有空位
+                    if len(res)>0 and venue["pid"] != res[0]["id"]:
+                        availability[time_slot] = "有"
+                    else:
+                        availability[time_slot] = ""
+                except requests.exceptions.RequestException as e:
+                    print(f"An error occurred for venue {venue['display_name']}, date {formatted_date}, time slot {time_slot}:", e)
+                    availability[time_slot] = "Error"
+            
+            # 将当天的空闲情况添加到表格中
+            availability_table.loc[len(availability_table)] = availability
+        
+        # 将该场地的空闲情况表格保存到字典中
+        availability_tables[venue["display_name"]] = availability_table
     
-    # 输出空闲时间段表格
-    print("\n接下来几天的羽毛球场空闲时间段如下:")
-    print(availability_table.transpose().to_markdown(index=True))
+    # 输出所有场地的空闲时间段表格
+    for venue_name, table in availability_tables.items():
+        print(f"\n接下来几天的{venue_name}空闲时间段如下:")
+        print(table.transpose().to_markdown(index=True))
     
     input("按回车键退出......")
 
